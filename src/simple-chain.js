806e6ae625a65189a7f2f52cc9c54cf5fbce9a10
neiloxx <anastasia.v.sido@gmail.com>
2021-02-14 18:47:09 +0600

    feat: add simple-chain function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
-  getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    result: [],
+    getLength() {
+        return this.result.length;
+    },
+    addLink(value) {
+        this.result.push(`( ${value} )`)
+        return this;
+    },
+    removeLink(position) {
+        if (typeof position === 'number' && position >= 0 && position < this.result.length) {
+            this.result.splice(position - 1, 1);
+        } else {
+            this.result = [];
+            throw new Error();
+        }
+        return this;
+    },
+    reverseChain() {
+        this.result.reverse();
+        return this;
+    },
+    finishChain() {
+        let tempRes = this.result.join('~~');
+        this.result = [];
+        return tempRes;
+    },
 };
 
 module.exports = chainMaker;
